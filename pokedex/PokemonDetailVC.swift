8f4dfc6b6ab8e9bacf870902925f7d17a627568b
Chase McElroy <ichasem@gmail.com>
2016-03-29 19:23:50 -0700

    Formatted detail view controller and linked text to code

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -11,6 +11,19 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     
     var pokemon: Pokemon!
     
@@ -25,7 +38,12 @@ class PokemonDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    
+    
     /*
     // MARK: - Navigation
 
