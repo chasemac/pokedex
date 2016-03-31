14fd94ecbdd2d8e0efb308b1d82316e33553b876
Chase McElroy <ichasem@gmail.com>
2016-03-31 15:50:47 -0700

    connected the http://pokeapi.co/ API and parsed JSON

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -31,6 +31,12 @@ class PokemonDetailVC: UIViewController {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+        pokemon.downloadPokemonDetails { () -> () in
+            //this will be called after download is done
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
